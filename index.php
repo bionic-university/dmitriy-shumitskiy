4e4b36bc01c3f3b0e9649931db9eaf6b88202620
Accept <aaccept2010@gmail.com>
2014-04-23 16:54:11 +0300

    refactoring

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
+/**
+*Программа считывает строку введенную с клавиатуры
+*и выводит статистику по частоте встречающихся сиволом
+*в введеной строке.
+*/
 echo ' Enter the string: ';
-$a = fgets(STDIN, 255);
-$l = strlen($a);
-	foreach(count_chars($a,1) as $b => $result){
-echo "You enter $result times the symbol \"",chr($b) ,"\" in the string $a.\n";
-	}
-
-echo "String length : $l";
+$enterString = fgets(STDIN, 255);
+$lengthString = strlen($enterString);
+foreach (count_chars ($enterString,1) as $b => $result){
+	echo "You enter $result times the symbol \"",chr($b) ,"\" in the string $enterString.\n";
+}
+echo "String length : $lengthString";
 ?>
 
