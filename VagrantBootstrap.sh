6147e7ba766079621dc1c26808b2039b8c4aa011
Acceptto <aaccept2010@gmail.com>
2014-05-26 15:26:46 +0300

    Build files for Jenkins

--- VagrantBootstrap.sh
+++ VagrantBootstrap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y php5-common php5-fpm php5-cli php5-curl php5-mysql php5-gd curl tree git nginx mysql-workbench php5-xdebug
+apt-get install -y php5-common php5-fpm php5-cli php5-curl php5-mysql php5-gd curl tree git nginx mysql-server mysql-client mysql-workbench php5-xdebug
