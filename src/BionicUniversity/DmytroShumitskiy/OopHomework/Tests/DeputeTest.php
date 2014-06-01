c594fadb402962c5b2810bff41010b75cd689346
Acceptto <aaccept2010@gmail.com>
2014-06-02 02:46:36 +0300

    bug in folders name

--- src/BionicUniversity/DmytroShumitskiy/OopHomework/Tests/DeputeTest.php
+++ src/BionicUniversity/DmytroShumitskiy/OopHomework/Tests/DeputeTest.php
@@ -7,7 +7,7 @@ use BionicUniversity\DmytroShumitskiy\OopHomework\President\Servants\Deputy;
 class DeputeTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testGetName()
+    public function testDeputeCreating()
     {
         $user = new Deputy('Ivan Ivanov', 35, 'men', 'KPI', 10, 'UA', 'Svoboda');
         $this->assertEquals(
