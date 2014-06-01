865a26f6535424c6a40af661a68850d5b31ea3cf
Acceptto <aaccept2010@gmail.com>
2014-06-02 02:56:15 +0300

    I wodering why assertEqual can test one param only(

--- src/BionicUniversity/DmytroShumitskiy/OopHomework/Tests/DeputeTest.php
+++ src/BionicUniversity/DmytroShumitskiy/OopHomework/Tests/DeputeTest.php
@@ -7,24 +7,9 @@ use BionicUniversity\DmytroShumitskiy\OopHomework\President\Servants\Deputy;
 class DeputeTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testDeputeCreating()
+    public function testGetName()
     {
         $user = new Deputy('Ivan Ivanov', 35, 'men', 'KPI', 10, 'UA', 'Svoboda');
-        $this->assertEquals(
-            'Ivan Ivanov',
-            35,
-            'men',
-            'KPI',
-            10,
-            'UA',
-            'Svoboda',
-            $user->getName(),
-            $user->getAge(),
-            $user->getGender(),
-            $user->getEducation(),
-            $user->getExperience(),
-            $user->getCitizenship(),
-            $user->getParty()
-        );
+        $this->assertEquals('Ivan Ivanov', $user->getName());
     }
 }
